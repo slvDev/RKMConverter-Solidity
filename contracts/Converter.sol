fae38800e4b4be3f8b0d4bbce27a5ba1dd4c447a
slvDev <slava.serbov@gmail.com>
2020-12-04 15:34:27 +0200

    refining contract code and tests

--- contracts/Converter.sol
+++ contracts/Converter.sol
@@ -5,24 +5,28 @@ contract Converter {
     function convert(uint _num) public pure returns (string memory) {
         bytes memory map = '0000KKKMMMGGGTTTPPPEEEZZZYYY';
         uint8 len;
-        // Calculate the length of the input number
-        uint i = _num;
+        
+        uint i = _num; // Calculate the length of the input number
         while (i != 0){
             len++;
             i /= 10;
         }
-        // Gets prefix Symbol
-        bytes1 char = map[len];
-        // Calculate position of symbol
-        uint8 charPos = len > 3 ? ((len - 1) % 3) + 1 : 0;
-        // Gets the first 4 number from input or leave as is if the number less than 1000
+        
+        bytes1 char = map[len]; // Get the prefix code letter
+        
+        uint8 charPos = len > 3 ? ((len - 1) % 3) + 1 : 0; // Position of prefix (or 0 if the number is 3 digits or less) 
+
+        // Gets the leftmost 4 digits from input number or just take the number as is if it's already 4 digits of less
         uint firstFour = len > 3 ? _num / 10 ** (len - 4) : _num;
+
         bytes memory bStr = "00000";
-        uint k = 4;
+        // We start from index 4 ^ of zero-string and go left
+        uint index = 4;
+
         while(firstFour != 0) {
-            // If symbol position is equal k, stores symbol in bytes and decrease k
-            if(k == charPos) bStr[k--] = char;
-            bStr[k--] = bytes1(uint8(48 + (firstFour % 10)));
+            // If index is on prefix position - insert a prefix and decrease index
+            if(index == charPos) bStr[index--] = char;
+            bStr[index--] = bytes1(uint8(48 + (firstFour % 10)));
             firstFour /= 10;
         }
         return string(bStr);
--- test/converter.js
+++ test/converter.js
@@ -11,71 +11,81 @@ describe("Converter", function() {
         await converter.deployed()
     })
 
-    it("Check num from 0 to 10**3 (kilo)", async function() {
+    it("Check num from 0 to 10**3", async function() {
         expect(await converter.convert(0)).to.eq("00000")
         expect(await converter.convert(1)).to.eq("00001")
         expect(await converter.convert(5)).to.eq("00005")
         expect(await converter.convert(54)).to.eq("00054")
         expect(await converter.convert(567)).to.eq("00567")
         expect(await converter.convert(999)).to.eq("00999")
-        expect(await converter.convert(1000)).to.eq("1K000")
     });
-
-    it("Check num from 10**3 to 10**6(mega)", async function() {
+    
+    it("Check num from 10**3 to 10**6(kilo)", async function() {
+        expect(await converter.convert(1000)).to.eq("1K000")
         expect(await converter.convert(1001)).to.eq("1K001")
         expect(await converter.convert(1985)).to.eq("1K985")
         expect(await converter.convert(76543)).to.eq("76K54")
         expect(await converter.convert(982553)).to.eq("982K5")
         expect(await converter.convert(999999)).to.eq("999K9")
-        expect(await converter.convert(1000000)).to.eq("1M000")
     });
-
-    it("Check num from 10**6 to 10**9(giga)", async function() {
+    
+    it("Check num from 10**6 to 10**9(mega)", async function() {
+        expect(await converter.convert(1000000)).to.eq("1M000")
         expect(await converter.convert(1000001)).to.eq("1M000")
         expect(await converter.convert(1800000)).to.eq("1M800")
         expect(await converter.convert(19500000)).to.eq("19M50")
         expect(await converter.convert(145800000)).to.eq("145M8")
         expect(await converter.convert(999999999)).to.eq("999M9")
-        expect(await converter.convert(1000000000)).to.eq("1G000")
     });
-
-    it("Check num from 10**9 to 10**12(teta)", async function() {
+    
+    it("Check num from 10**9 to 10**12(giga)", async function() {
+        expect(await converter.convert(1000000000)).to.eq("1G000")
         expect(await converter.convert(1000000001)).to.eq("1G000")
         expect(await converter.convert(1900000000)).to.eq("1G900")
         expect(await converter.convert(16500000000)).to.eq("16G50")
         expect(await converter.convert(112500000000)).to.eq("112G5")
         expect(await converter.convert(999999999999)).to.eq("999G9")
-        expect(await converter.convert(1000000000000)).to.eq("1T000")
     });
-
-    it("Check num from 10**12 to 10**15(peta)", async function() {
+    
+    it("Check num from 10**12 to 10**15(teta)", async function() {
+        expect(await converter.convert(1000000000000)).to.eq("1T000")
         expect(await converter.convert(1000000000001)).to.eq("1T000")
         expect(await converter.convert(1200000000000)).to.eq("1T200")
         expect(await converter.convert(18900000000000)).to.eq("18T90")
         expect(await converter.convert(148600000000000)).to.eq("148T6")
         expect(await converter.convert(999999999999999)).to.eq("999T9")
-        expect(await converter.convert(1000000000000000)).to.eq("1P000")
     });
-
-    it("Check num from 10**15 to 10**18(exa)", async function() {
+    
+    it("Check num from 10**15 to 10**18(peta)", async function() {
+        expect(await converter.convert(1000000000000000)).to.eq("1P000")
         expect(await converter.convert(1000000000000001)).to.eq("1P000")
         expect(await converter.convert(1500000000000000)).to.eq("1P500")
         expect(await converter.convert(BigNumber.from('82500000000000000'))).to.eq("82P50")
         expect(await converter.convert(BigNumber.from('952500000000000000'))).to.eq("952P5")
         expect(await converter.convert(BigNumber.from('999999999999999999'))).to.eq("999P9")
-        expect(await converter.convert(BigNumber.from('1000000000000000000'))).to.eq("1E000")
     });
-
-    it("Check num from 10**18 to 10**21(zetta)", async function() {
+    
+    it("Check num from 10**18 to 10**21(exa)", async function() {
+        expect(await converter.convert(BigNumber.from('1000000000000000000'))).to.eq("1E000")
         expect(await converter.convert(BigNumber.from('1000000000000000001'))).to.eq("1E000")
         expect(await converter.convert(BigNumber.from('6500000000000000000'))).to.eq("6E500")
         expect(await converter.convert(BigNumber.from('84160000000000000000'))).to.eq("84E16")
         expect(await converter.convert(BigNumber.from('349500000000000000000'))).to.eq("349E5")
         expect(await converter.convert(BigNumber.from('999999999999999999999'))).to.eq("999E9")
+    });
+    
+    it("Check num from 10**18 to 10**24(zetta)", async function() {
         expect(await converter.convert(BigNumber.from('1000000000000000000000'))).to.eq("1Z000")
+        expect(await converter.convert(BigNumber.from('1000000000000000000001'))).to.eq("1Z000")
+        expect(await converter.convert(BigNumber.from('9500000000000000000000'))).to.eq("9Z500")
+        expect(await converter.convert(BigNumber.from('69800000000000000000000'))).to.eq("69Z80")
+        expect(await converter.convert(BigNumber.from('964300000000000000000000'))).to.eq("964Z3")
+        expect(await converter.convert(BigNumber.from('999999999999999999999999'))).to.eq("999Z9")
+        expect(await converter.convert(BigNumber.from('1000000000000000000000000'))).to.eq("1Y000")
     });
 
     it("Check num from 10**18 to 10**24(yotta)", async function() {
+        expect(await converter.convert(BigNumber.from('1000000000000000000000'))).to.eq("1Z000")
         expect(await converter.convert(BigNumber.from('1000000000000000000001'))).to.eq("1Z000")
         expect(await converter.convert(BigNumber.from('9500000000000000000000'))).to.eq("9Z500")
         expect(await converter.convert(BigNumber.from('69800000000000000000000'))).to.eq("69Z80")
